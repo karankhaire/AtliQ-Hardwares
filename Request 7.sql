cd511ff4cfe49afca41f125f2e9fca9ea4a4a178
Karan Khaire <149904973+karankhaire@users.noreply.github.com>
2024-08-08 15:37:11 +0530

    Update Request 7.sql

--- Request 7.sql
+++ Request 7.sql
@@ -1,7 +1,7 @@
 #7 Get the complete report of the Gross sales amount for the customer “Atliq Exclusive” for each month.
 
 SELECT
-MONTH(date) AS month_name,
+MONTH(date) AS month_,
 YEAR(date) AS year_,
 ROUND(SUM(gross_price*sold_quantity)/1000000,2) AS Gross_sales_Amount
 FROM fact_sales_monthly AS a
